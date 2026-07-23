cd71265a8b6916d1cd19498142d9549d0c21f340
amahacker001 <aliyu09013852501@gmail.com>
2026-07-23 15:34:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-const category = document.getElementById("category").value;
+function addExpense() {
+  const name = document.getElementById("name").value;
+  const amount = document.getElementById("amount").value;
+  const category = document.getElementById("category").value;
 
-expenses.push({
-  name,
-  amount,
-  category
-});
+  if (!name || !amount) return;
+
+  expenses.push({
+    name,
+    amount,
+    category
+  });
 
   let list = document.getElementById("list");
-
   let li = document.createElement("li");
 
   li.innerHTML = `
-    ${name} - ₦${amount}
+    ${name} (${category}) - ₦${amount}
     <button onclick="deleteExpense(this)">❌</button>
   `;
 
   list.appendChild(li);
+}
 
   // Update total
   let total = document.getElementById("total");
