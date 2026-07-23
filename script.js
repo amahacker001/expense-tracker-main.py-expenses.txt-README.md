502db268f44ee98dff8574676b9197d8bb662c62
amahacker001 <aliyu09013852501@gmail.com>
2026-07-23 14:16:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+let expenses = JSON.parse(localStorage.getItem("expenses")) || [];
+
+function displayExpenses() {
+    let list = document.getElementById("list");
+    list.innerHTML = "";
+
+    expenses.forEach((exp, index) => {
+        let li = document.createElement("li");
+        li.textContent = exp.name + " - ₦" + exp.amount;
+
+        list.appendChild(li);
+    });
+}
+
 function addExpense() {
     let name = document.getElementById("name").value;
     let amount = document.getElementById("amount").value;
@@ -7,11 +21,14 @@ function addExpense() {
         return;
     }
 
-    let li = document.createElement("li");
-    li.textContent = name + " - ₦" + amount;
+    expenses.push({ name, amount });
 
-    document.getElementById("list").appendChild(li);
+    localStorage.setItem("expenses", JSON.stringify(expenses));
 
     document.getElementById("name").value = "";
     document.getElementById("amount").value = "";
+
+    displayExpenses();
 }
+
+window.onload = displayExpenses;
