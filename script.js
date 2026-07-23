8b3085fd0c56a25940ed5d8f4d45d2283eb83065
amahacker001 <aliyu09013852501@gmail.com>
2026-07-23 18:24:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -73,3 +73,18 @@ function loadExpenses() {
 // RUN WHEN PAGE LOADS
 loadExpenses();
 document.getElementById("addBtn").addEventListener("click", addExpense);
+function downloadCSV() {
+  let csv = "Name,Category,Amount\n";
+
+  expenses.forEach(expense => {
+    csv += `${expense.name},${expense.category},${expense.amount}\n`;
+  });
+
+  const blob = new Blob([csv], { type: "text/csv" });
+  const url = window.URL.createObjectURL(blob);
+
+  const a = document.createElement("a");
+  a.setAttribute("href", url);
+  a.setAttribute("download", "expenses.csv");
+  a.click();
+}
